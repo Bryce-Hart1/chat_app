72be8804302f693430c54d41a1dfb809cede25d5
Bryce Hart <bryce.hart23@gmail.com>
2026-04-16 20:47:39 -0400

    Small improvements on readability

--- client.py
+++ client.py
@@ -2,38 +2,39 @@ import socket
 import random
 
 
-LOCAL_HOST: str = '127.0.0.1'
+LOCAL_HOST: str = '127.0.0.1' #hardcoded to be cleaner, same w server
+DEFAULT_PORT: int = 8080
 
-
-client_IP: str = input("Please enter a serverIP, or enter for enter for localhost")
+#for the initial setup added line break to read cleaner
+client_IP: str = input("Please enter a serverIP, or enter for enter for localhost \n")
 
 if client_IP == "":
-    server_IP = LOCAL_HOST
+    client_IP = LOCAL_HOST
 
 
 
 #we are gonna get port (keep as int and use that for the rest)
 while True: 
-    port_input: str = input("Please enter a port number")
+    port_input: str = input("Please enter a port number \n")
     if port_input == "":
-        port: int = 8080 #if its not a valid port, default
+        port: int = DEFAULT_PORT #if its not a valid port, default
         print("Valid port was not entered, defaulted to port 8080") 
         break
     elif port_input.isdigit() and 1024 <= int(port_input) <= 65535: #has to be between if not:
-        port: int = int(port_input) 
+        port = int(port_input) 
         break
     else: 
         print("Port is invalid. Try again, with the number between 1024 and 65535") #reset
 
 
 
-requested_username: str = input("Please enter a username: ")
+requested_username: str = input("Please enter a username: \n")
 
 if requested_username == "":
     random_appended: int = random.randint(100, 999)
     default_username: str = f"user{random_appended}" 
     # just give a default name
-    print(f"Username was not entered. Username is {default_username}")
+    print(f"Username was not entered. Username is: {default_username}")
     requested_username = default_username
 
 
@@ -44,7 +45,7 @@ try:
     client_socket.connect((client_IP, port))
     print(f"Connected to server at {client_IP} | {port}")
 except ConnectionRefusedError:
-    print("Connection refused. Make sure the server program is running.")
+    print("Connection refused. Make sure the server program is running. \n")
     client_socket.close()
     exit()
 except OSError as e:
@@ -53,18 +54,18 @@ except OSError as e:
     exit()
 
 
-# Receive server's username first
+# Receive server username first
 server_username = client_socket.recv(1024).decode()
-print(f"Connected! Chatting with {server_username}") # now we bring in servers username and display
+print(f"Connected! chatting with: {server_username}") # now we bring in servers username and display
 
-# Send client's username to server
+# Send client username to server
 client_socket.send(requested_username.encode())
 
 while True:
     try:
         message = input(f"{requested_username}: ").strip() # take the whitespace off the message
         client_socket.send(message.encode())
-        if message.lower() == "end":
+        if message.lower() == "end": #lower it just in case for simplicity
             print("Ending chat.")
             break
 
